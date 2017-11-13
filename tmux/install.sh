9f16f64384a322d08ee92e420f386f1f3030b1cb
mcmonkeyman <eoinoc.mail@gmail.com>
2017-11-12 20:28:15 -0500

    Update install script

--- tmux/install.sh
+++ tmux/install.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 # Get powerlevel fonts: https://github.com/powerline/fonts
-if cd ~/code/fonts; then git pull; else git clone https://github.com/powerline/fonts.git --depth=1; fi
+if cd ~/code/fonts; then git pull; else git clone https://github.com/powerline/fonts.git ~/code/fonts --depth=1; fi
 ~/code/fonts/install.sh
