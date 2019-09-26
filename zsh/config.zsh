e1dcc51c1c54d16d610be99fbccc7f51565ea666
Eoin <eoin4real@gmail.com>
2019-09-25 21:07:52 -0400

    Add control+-> for command movement

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -43,7 +43,3 @@ bindkey '^?' backward-delete-char
 bindkey -e
 bindkey '^[[1;9C' forward-word
 bindkey '^[[1;9D' backward-word
-
-bindkey -e
-bindkey '^[[1;9C' forward-word
-bindkey '^[[1;9D' backward-word
